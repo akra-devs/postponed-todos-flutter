ae6bb0769968b8d3c04c2d2b80a5210599ec632d
refinedstone <chem.en.9273@gmail.com>
2026-04-06 13:57:39 +0900

    chore: tune completion reward throttle cooldown to 45 minutes

--- lib/core/config/product_policy_defaults.dart
+++ lib/core/config/product_policy_defaults.dart
@@ -12,7 +12,7 @@ class ProductPolicyDefaults {
   static const int homeDefaultCards = 4;
 
   static const Duration completionRewardThrottleCooldown = Duration(
-    minutes: 30,
+    minutes: 45,
   );
 
   static Duration cooldownForSnoozeCount(int consecutiveSnoozeCount) {
